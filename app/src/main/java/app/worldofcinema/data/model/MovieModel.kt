cde3878fca1898482a620cdd149eff89e4ba3862
Gerlenn <gleb_kozlov1998@mail.ru>
2023-02-22 13:18:46 +0300

    Added to the project: retrofit2, MoviesFragment()

---
+++ app/src/main/java/app/worldofcinema/data/model/MovieModel.kt
@@ -0,0 +1,9 @@
+package app.worldofcinema.data.model
+
+data class MovieModel(
+    val id: String,
+    val imDbRating: String,
+    val image: String,
+    val title: String,
+    val year: String,
+)
